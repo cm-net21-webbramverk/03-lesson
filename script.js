7e655ca78811d5c95a788b901d04faf56cf73993
David Andersson <lejonmanen@gmail.com>
2022-03-17 14:25:27 +0100

    Restore border

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let section1 = document.querySelector('section')
-let section2 = document.querySelector('main > :nth-child(3)')
+let section2 = document.querySelector('main > :nth-child(5)')
 
 section1.innerText = '<strong>Hello</strong> world - innerText lägger bara in text'
 
@@ -20,11 +20,18 @@ for( let i=1; i<=10; i++ ) {
 let main = document.querySelector('main')
 main.appendChild(lista)
 
+let sections = document.querySelectorAll('section')
 let borderButton = document.querySelector('#border-button')
 borderButton.addEventListener('click', event => {
 	// console.log('event object: ', event)
-	let sections = document.querySelectorAll('section')
 	sections.forEach(s => {
 		s.className = 'modified-border'
+	})
+})
+let restoreButton = document.querySelector('#restore-button')
+restoreButton.addEventListener('click', event => {
+	// console.log('event object: ', event)
+	sections.forEach(s => {
+		s.className = ''
 	})
 })
